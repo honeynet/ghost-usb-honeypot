8e02ff8320a6b998ab072f4769784db2fd2a98d2
Sebastian Poeplau <sebastian.poeplau@googlemail.com>
2012-08-21 12:09:42 +0200

    The frontend can now control automatic and manual operation.

--- GhostGUI/MainForm.cs
+++ GhostGUI/MainForm.cs
@@ -11,25 +11,136 @@ namespace GhostGUI
 {
     public partial class MainForm : Form
     {
-        private Ghost MainGhost = new Ghost();
+        private List<Ghost> Ghosts = new List<Ghost>();
+        private Ghost MainGhost;
+        private delegate void ViewUpdater();
 
         public MainForm()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void buttonMount_Click(object sender, EventArgs e)
         {
+            MainGhost = new Ghost();
+            Ghosts.Add(MainGhost);
+
+            MainGhost.OnUpdate += ThreadSafeUpdate;
             MainGhost.Start();
+
+            buttonMount.Enabled = false;
+            buttonUmount.Enabled = true;
+            radioButtonAutomatic.Enabled = false;
+            radioButtonManual.Enabled = false;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void buttonUmount_Click(object sender, EventArgs e)
         {
             MainGhost.Stop();
-            foreach (Incident i in MainGhost.IncidentList)
+            MainGhost = null;
+
+            buttonUmount.Enabled = false;
+            buttonMount.Enabled = true;
+            radioButtonManual.Enabled = true;
+            radioButtonAutomatic.Enabled = true;
+        }
+
+        private void ThreadSafeUpdate()
+        {
+            if (treeViewResults.InvokeRequired)
             {
-                MessageBox.Show(i.ToString());
+                ViewUpdater Updater = UpdateTreeView;
+                treeViewResults.Invoke(Updater);
             }
+            else
+            {
+                UpdateTreeView();
+            }
+        }
+
+        private void UpdateTreeView()
+        {
+            treeViewResults.BeginUpdate();
+            treeViewResults.Nodes.Clear();
+
+            foreach (Ghost g in Ghosts)
+            {
+                if (g.MountTime == null)
+                {
+                    continue;
+                }
+
+                TreeNode NewNode = treeViewResults.Nodes.Add(g.MountTime.ToString());
+                foreach (Incident i in g.IncidentList)
+                {
+                    TreeNode SubNode = NewNode.Nodes.Add("PID: " + i.PID.ToString() + ", TID: " + i.TID.ToString());
+                    if (i.Modules.Count != 0)
+                    {
+                        TreeNode Modules = SubNode.Nodes.Add("Loaded Modules");
+                        foreach (String m in i.Modules)
+                        {
+                            Modules.Nodes.Add(m);
+                        }
+                    }
+                }
+            }
+
+            treeViewResults.EndUpdate();
+        }
+
+        private void radioButtonManual_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonManual.Checked)
+            {
+                buttonMount.Enabled = true;
+                buttonUmount.Enabled = false;
+                numericMountInterval.Enabled = false;
+                numericMountDuration.Enabled = false;
+                timerGhost.Enabled = false;
+            }
+            else
+            {
+                buttonMount.Enabled = false;
+                buttonUmount.Enabled = false;
+                numericMountInterval.Enabled = true;
+                numericMountDuration.Enabled = true;
+                timerGhost.Enabled = true;
+            }
+        }
+
+        private void numericMountInterval_ValueChanged(object sender, EventArgs e)
+        {
+            timerGhost.Interval = (int) numericMountInterval.Value * 60000;
+        }
+
+        private void timerGhost_Tick(object sender, EventArgs e)
+        {
+            timerGhost.Stop();
+
+            radioButtonAutomatic.Enabled = false;
+            radioButtonManual.Enabled = false;
+            labelAutomaticInProgress.Show();
+
+            MainGhost = new Ghost();
+            Ghosts.Add(MainGhost);
+            MainGhost.OnUpdate += ThreadSafeUpdate;
+
+            MainGhost.Start();
+
+            timerUmount.Interval = (int)numericMountDuration.Value * 1000;
+            timerUmount.Start();
+        }
+
+        private void timerUmount_Tick(object sender, EventArgs e)
+        {
+            timerUmount.Stop();
+            MainGhost.Stop();
+
+            labelAutomaticInProgress.Hide();
+            radioButtonAutomatic.Enabled = true;
+            radioButtonManual.Enabled = true;
+
+            timerGhost.Start();
         }
     }
 }
